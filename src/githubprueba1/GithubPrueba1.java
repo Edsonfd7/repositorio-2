5212f307d285073d66b3c1e07eb45d53f7dd0c02
Hp <Hp@DESKTOP-QSE36DP.local>
2022-11-17 23:59:26 -0600

    prueba repositorio 2

---
+++ src/githubprueba1/GithubPrueba1.java
@@ -0,0 +1,26 @@
+/*
+ */
+package githubprueba1;
+
+/**
+ */
+public class GithubPrueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a=10,b=5;
+       int suma=a+b;
+       int resta=a-b;
+       int multiplicacion=a*b;
+       int divi=a/b;
+        System.out.println("ejemplo github");
+        
+        System.out.println("la suma es "+suma);
+        System.out.println("la resta es "+resta);
+        System.out.println("la multiplicacion es "+multiplicacion);
+        System.out.println("la division es "+divi);
+    }
+    
+}
